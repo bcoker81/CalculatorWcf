baa22dcd29307c7b389d501ef985a3697415d8a6
Bcoker81 <branden.coker@gmail.com>
2018-03-17 10:04:27 -0500

    set up test project

---
+++ Calculator/Services/Register.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Calculator.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Register" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Register.svc or Register.svc.cs at the Solution Explorer and start debugging.
+    public class Register : IRegister
+    {
+        public int AddTwoNumbers(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ CalculatorTests/RegisterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Services.Tests
+{
+    [TestClass()]
+    public class RegisterTests
+    {
+        [TestMethod()]
+        public void AddTwoNumbersTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
